480a215122708348ab74cc5d368468ad436230ea
Shahrukh037 <animeparadise37@gmail.com>
2025-12-09 15:34:43 +0530

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase v8 compatible config
+var firebaseConfig = {
+  apiKey: "AIzaSyCXCKn5zDjbwDHwdEROGrKGPxpok7Yndlw",
+  authDomain: "styloxio.firebaseapp.com",
+  projectId: "styloxio",
+  storageBucket: "styloxio.appspot.com",
+  messagingSenderId: "1006696685386",
+  appId: "1:1006696685386:web:a1b5423a59c97b3972abb3"
+};
+
+// Initialize Firebase (v8 syntax)
+firebase.initializeApp(firebaseConfig);
+
+// Global handles
+const auth = firebase.auth();
+const db = firebase.firestore();
